293ca294c9392a4e71836431c4d5d9bb6ca6d39c
MistressHorse <sofiazhguleva131@gmail.com>
2026-02-13 20:53:29 +0300

    Update engine.py

--- database/engine.py
+++ database/engine.py
@@ -2,7 +2,9 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DB_PATH = 'database.db'
+# DB_PATH = 'database.db'
+DB_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'database.db')
+
 
 engine = create_async_engine(f"sqlite+aiosqlite:///{DB_PATH}", echo=True)
 
